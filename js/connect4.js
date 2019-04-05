b40a69edb2dec183bafa15ee90f1fce71786ffe5
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-04-04 23:12:03 -0700

    alternate color cell where clicked and stack up from the bottom

--- js/connect4.js
+++ js/connect4.js
@@ -3,6 +3,7 @@ class Connect4 {
     this.rows = 6;
     this.columns = 7;
     this.connector = connector;
+    this.player = "redPlayer";
     this.createGrid();
     this.eventListeners();
   }
@@ -27,12 +28,15 @@ class Connect4 {
   // set up event listeners
   eventListeners() {
     const $gameboard = $(this.connector);
+    // get access to the original this attr
+    const that = this;
     function findLastEmptyCell(col) {
       // get
       const cells = $(`.col[data-col='${col}']`);
       // looping cells backwards
       for (let i = cells.length - 1; i >= 0; i--) {
         const $cell = $(cells[i]);
+        // any cells that has empty class
         if ($cell.hasClass("empty")) {
           return $cell;
         }
@@ -44,12 +48,25 @@ class Connect4 {
       const col = $(this).data("col");
       const $lastEmptyCell = findLastEmptyCell(col);
       // add class when mouse hovers
-      $lastEmptyCell.addClass(`empty-one`);
+      $lastEmptyCell.addClass(`empty-${that.player}`);
       //   console.log(col);
     });
     // remove class when mouse leaves
     $gameboard.on("mouseleave", ".col", function() {
-      $(".col").removeClass(`empty-one`);
+      $(".col").removeClass(`empty-${that.player}`);
+    });
+    $gameboard.on("click", ".col.empty", function() {
+      const col = $(this).data("col");
+      const $lastEmptyCell = findLastEmptyCell(col);
+      // remove the class empty when click
+      $lastEmptyCell.removeClass("empty");
+      // add color class
+      $lastEmptyCell.addClass(that.player);
+      if (that.player === "redPlayer") {
+        that.player = "blackPlayer";
+      } else {
+        that.player = "redPlayer";
+      }
     });
   }
 }
